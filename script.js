dc026fc9f6b416db3ed6f86075bfbd67d2d343a7
Nikodemus Karlsson <nikodemus@nikodemus.se>
2022-03-23 21:16:48 +0100

    file update

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // Några grundläggande inställningar
 let canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
-canvas.height = 0.8 * window.innerHeight;
+canvas.height = 1 * window.innerHeight;
+
+canvas.style.width = "90%";
+canvas.style.height = "90%";
 
 // Genom variabeln c kommer man åt det mesta som
 // med canvas att göra
@@ -11,9 +14,9 @@ function paintSquare() {
   // Blå kvadrat
   c.fillStyle = "blue";
   c.fillRect(canvas.width / 2 - 200, canvas.height / 2 - 200, 400, 400);
-  //c.fillRect(0, 0, 50, 50);
+  // c.fillRect(0, 0, 50, 50); // Origo
 
-  c.font = "60px Arial";
+  c.font = "48px Arial";
   c.fillStyle = "Yellow";
   c.textAlign = "center";
   c.fillText("Canvas Rect", canvas.width / 2, canvas.height / 2);
@@ -23,23 +26,44 @@ function paintCircle() {
   // Gul cirkel
   c.fillStyle = "yellow";
   c.beginPath();
+  // c.arc(x, y, radie, startvinkel, slutvinkel)
   c.arc(canvas.width / 2, canvas.height / 2, 150, 0, 2 * Math.PI); // En cirkel är en "arc" som går mellan 0 och 2pi
   c.closePath();
   c.fill();
 }
 
+function paintRings() {
+  console.log(canvas.width, canvas.height);
+  console.log(400 + 100 * Math.cos((Math.PI * 45) / 180));
+  c.fillStyle = "red";
+  for (let i = 1; i <= 12; i++) {
+    c.beginPath();
+    c.arc(
+      canvas.width / 2 + 200 * Math.cos((((Math.PI * 360) / 12) * i) / 180),
+      canvas.height / 2 + 200 * Math.sin((((Math.PI * 360) / 12) * i) / 180),
+      15,
+      0,
+      2 * Math.PI
+    );
+    c.closePath();
+    c.fill();
+  }
+}
+
 function paintSimpleSquarePattern() {
   let color = ["blue", "red"];
-  for (let row = 0; row <= 10; row++) {
-    for (let col = 1; col <= 5; col++) {
+  for (let row = 0; row <= 8; row++) {
+    for (let col = 0; col <= 8; col++) {
       c.fillStyle = color[row % 2];
       if (col % 2 === 1) {
         c.fillStyle = color[(row + 1) % 2];
       }
-      c.fillRect(row * 100 + 400, col * 100 + 50, 50, 50);
+      c.fillRect(row * 110 + 5, col * 110 + 5, 40, 40);
     }
   }
 }
+
 paintSquare();
 // paintCircle();
 // paintSimpleSquarePattern();
+// paintRings();
