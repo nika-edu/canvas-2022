1e5ef5666a728219f232eb5f5006cb9178f66348
Nikodemus Karlsson <nikodemus@nikodemus.se>
2022-03-24 17:58:25 +0100

    file update

---
+++ README.md
@@ -0,0 +1,18 @@
+# Bekanta dig med canvas
+
+En `canvas` är ett HTML-objekt som det går att "måla" t ex geometriska figurer på, skriva text eller göra animeringar på. Det styrs från en JavaScript-fil och hela området är koordinatmappat.
+
+JavaScript-filen ovan demonstrerar några grundläggande figurer och hur det går till att layouta en `canvas`. Tanken är att du själv ska pröva på det genom att i första hand göra några förändringar i denna givna JavaScript-fil.
+
+1.  I slutet av filen `script.js` finns några funktionsanrop: en funktion som skapar en kvadrat med text i mitten på sidan, en som ritar en cirkelskiva, en som ritar ett mönster med många små kvadrater med växlande färg och en som ritar punkter på en cirkel (punkterna består i sin tur av små cirklar).
+
+    Se till att det enbart är kvadraten med dess text som syns på sidans `canvas`. Pröva att flytta kvadraten till olika ställen. Speciellt ska du se var origo ligger och vilken del av kvadraten som lägger sig på det givna läget. Pröva också att ändra storlek och färg på kvadraten.
+
+2.  En cirkel ritas som en "fylld sektor", där sektorns vinkel är 360 grader (eller egentligen 2\*pi). Pröva några olika start- och slutvinklar på den fyllda sektorn för att skapa olika figurer.
+
+3.  Funktionen `paintSimpleSquarePattern` ritar ett mönster med lite mindra kvadrater. Experimentera med antalet rader och kolumner och försök att lägga till en tredje färg som ingår i färgväxlingen.
+
+4.  Funktionen `paintRing` ritar punkter som ligger på en cirkel. I originalutförandet så är dessa "punkter" 12 st. cirkelskivor. Pröva att ändra antalet på dessa punkter så att de fortfarande ligger symmetriskt. Pröva också att skapa flera cirklar med olika radie och färg, jag tänker mig något i stil med nedanstående bild.
+    ![concentric circles](img/concentricCircles.png)
+
+            (Behöver du hjälp? Tjuvkika på [lösningsförslaget](https://gist.github.com/nika-edu/70b5ea2ed898c06b43247039737a0cd8))
--- script.js
+++ script.js
@@ -8,62 +8,105 @@ canvas.style.height = "90%";
 
 // Genom variabeln c kommer man åt det mesta som
 // med canvas att göra
-let c = canvas.getContext("2d");
+const c = canvas.getContext("2d");
+const midX = canvas.width / 2;
+const midY = canvas.height / 2;
+
+// Matar ut canvas dimensioner till konsolen, pröva att
+// ändra storlekn på fönstret och ladda däörefter om sidan.
+console.log(
+  `Bredd på canvas: ${canvas.width},
+Höjd på canvas: ${canvas.height}`
+);
 
 function paintSquare() {
   // Blå kvadrat
+  const s = 400; // Kvadratens sidlängd
   c.fillStyle = "blue";
-  c.fillRect(canvas.width / 2 - 200, canvas.height / 2 - 200, 400, 400);
-  // c.fillRect(0, 0, 50, 50); // Origo
+  c.fillRect(midX - s / 2, midY - s / 2, s, s);
 
+  // Textinställningar
   c.font = "48px Arial";
   c.fillStyle = "Yellow";
   c.textAlign = "center";
-  c.fillText("Canvas Rect", canvas.width / 2, canvas.height / 2);
 }
 
 function paintCircle() {
   // Gul cirkel
+  const r = 150; // Cirkelns radie
   c.fillStyle = "yellow";
   c.beginPath();
   // c.arc(x, y, radie, startvinkel, slutvinkel)
-  c.arc(canvas.width / 2, canvas.height / 2, 150, 0, 2 * Math.PI); // En cirkel är en "arc" som går mellan 0 och 2pi
+  c.arc(midX, midY, r, 0, 2 * Math.PI); // En cirkel är en "arc" som går mellan 0 och 2pi
   c.closePath();
   c.fill();
 }
 
-function paintRings() {
-  console.log(canvas.width, canvas.height);
-  console.log(400 + 100 * Math.cos((Math.PI * 45) / 180));
+function paintRing() {
+  // Funktionen ritar en ring som består av 12 jämnt fördelade prickar
+  const circleRadius = 200;
+  const dotRadius = 15;
   c.fillStyle = "red";
+  let x; // Kommer att lagra respektive pricks läge i x-led
+  let y; // Kommer att lagra respektive pricks läge i y-led
   for (let i = 1; i <= 12; i++) {
+    // Beräknar läget på resp. prick
+    x = midX + circleRadius * Math.cos((((Math.PI * 360) / 12) * i) / 180);
+    y = midY + circleRadius * Math.sin((((Math.PI * 360) / 12) * i) / 180);
+
+    // Ritar prickarna
     c.beginPath();
-    c.arc(
-      canvas.width / 2 + 200 * Math.cos((((Math.PI * 360) / 12) * i) / 180),
-      canvas.height / 2 + 200 * Math.sin((((Math.PI * 360) / 12) * i) / 180),
-      15,
-      0,
-      2 * Math.PI
-    );
+    c.arc(x, y, dotRadius, 0, 2 * Math.PI);
     c.closePath();
     c.fill();
   }
 }
 
-function paintSimpleSquarePattern() {
-  let color = ["blue", "red"];
-  for (let row = 0; row <= 8; row++) {
-    for (let col = 0; col <= 8; col++) {
-      c.fillStyle = color[row % 2];
-      if (col % 2 === 1) {
-        c.fillStyle = color[(row + 1) % 2];
-      }
-      c.fillRect(row * 110 + 5, col * 110 + 5, 40, 40);
+function paintManyRings() {
+  const n = 30; // Antal prickar som varje ring består av
+  const dotRadius = 12; // Storleken på prickarna
+  const dr = 50; // Avståndet mellan ringarna
+  const r0 = 15; // Den innersta ringens radie
+  const numRings = 10;
+  const colors = ["yellow", "blue", "red"];
+  let r = r0;
+  let x;
+  let y;
+  for (let i = 1; i <= numRings; i++) {
+    // Loop som ritar respektve ring
+    for (let j = 1; j <= n; j++) {
+      // Loop som ritar prickarna
+      r = r0 + dr * i; // Ringarnas radie ökar
+      x = midX + r * Math.cos((2 * Math.PI * j) / n); // Läget i x-led för en prick
+      y = midY + r * Math.sin((2 * Math.PI * j) / n); // Läget i y-led för en prick
+      c.beginPath(); // En prick är en liten cirkel
+      c.arc(x, y, dotRadius, 0, 2 * Math.PI, true);
+      c.fillStyle = colors[i % 3]; // Färgen bestäms
+      c.closePath();
+      c.fill(); // Pricken ritas ut
     }
   }
 }
 
-paintSquare();
+function paintSimpleSquarePattern() {
+  const s = 40; // Kvadratens sida
+  const space = 3 * s;
+  const numRows = 6;
+  const numCols = 10;
+  let color = ["blue", "red"];
+  for (let i = 0; i <= numCols; i++) {
+    for (let j = 0; j <= numRows; j++) {
+      c.fillStyle = color[(i + 1) % 2];
+      if (j % 2 === 1) {
+        c.fillStyle = color[i % 2];
+      }
+      c.fillRect(i * space, j * space, s, s);
+    }
+  }
+}
+
+// paintSquare();
 // paintCircle();
 // paintSimpleSquarePattern();
-// paintRings();
+// paintRing();
+paintManyRings();
